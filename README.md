5703786bf854bebf74e5ea8035ca48621097cd93
PhiHaxor <github_fc@aconti.com>
2019-12-13 10:31:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-q3zi2g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-q3zi2g)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = '<h1>JS Starter</h1>';
+
+appDiv.innerHTML = '<p> this is a test for phi - js-q3zi2g </p>';
+
